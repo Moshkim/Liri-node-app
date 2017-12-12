2b5b9382b77e50e1e1bb38cc83ccc73b6a922975
Moses <rlakim@ucdavis.edu>
2017-12-12 11:12:05 -0800

    Everything works except twitter is not getting answer that the authentication has problem

---
+++ keys.js
@@ -0,0 +1,19 @@
+
+
+//console.log("This is loaded!")
+
+var twitterKeys = {
+    consumer_key: 'vr1UX1T7jhRMucpCTbBikvlci',
+    consumer_secret: 'vqmUupd40wC1E06qPlw9NHY5AoxmZmqss1JwLj5r3BBWBHU43v',
+    access_token_key: '940275995424129026-ECdZA0vUQ4lexUbv7Ly1CMmyevCJCbW',
+    access_token_secret: '2wHnqQz7stvy1RBomI0r2womJfw6c1f8u2D8r23pyeSL3',
+}
+
+
+var spotifyKeys = {
+    id: "0e38a602f05b4a4a9cdc93e9e71a3cf3",
+    secret: "6a8c3314189c4ff38a17659e7d4a566c"
+}
+
+module.exports = twitterKeys
+module.exports = spotifyKeys
---
+++ liri.js
@@ -0,0 +1,186 @@
+
+
+var Keys = require('./keys.js')
+var Twitter = require('twitter')
+var Spotify = require('node-spotify-api')
+var request = require('request')
+var fs = require('fs')
+
+var client = new Twitter({
+    consumer_key: Keys.consumer_key,
+    consumer_secret: Keys.consumer_secret,
+    access_token_key: Keys.access_token_key,
+    access_token_secret: Keys.access_token_secret
+})
+
+
+var spotify = new Spotify({
+    id: Keys.id,
+    secret: Keys.secret
+})
+
+var commend = process.argv[2]
+var nameOfSongOrMovie = process.argv[3]
+var globalOrder
+
+order(commend)
+
+function order(commend) {
+    switch (commend) {
+        case "my-tweets":
+        myTweet()
+        break
+        case "spotify-this-song":
+        aboutSong()
+        break
+        case "movie-this":
+        myMovie()
+        break
+        case "do-what-it-says":
+        doWhatItSay()
+        break
+        default:
+        break
+    }
+}
+
+
+
+
+function myTweet() {
+    client
+    .get('/statuses/user_timeline.json', { count: 5, screen_name: 'tweets' }, function(error, tweets, response){
+        console.log("We have requested and getting some data")
+        if(error){
+            console.log(error)
+        } else {
+            console.log(tweets)
+            logging('my-tweets', 'nohing', tweets)
+        }
+    })
+}
+
+
+function aboutSong() {
+    if(nameOfSongOrMovie){
+        spotifyFunc(nameOfSongOrMovie)
+        
+    } else {
+        spotifyFunc(globalOrder)
+    }
+}
+
+function spotifyFunc(title){
+    spotify
+    .search({ type: 'track', query: title, limit:1 })
+    .then(function(response) {
+        var songLists = response.tracks.items
+        //var songListsObj = []
+        for(var i = 0; i < songLists.length; i++){
+            var info = "Artist(s): " + songLists[i].album.artists[0].name +
+            "\nName: " + songLists[i].name +
+            "\nPreview URL: " + songLists[i].preview_url +
+            "\nAlbum: " + songLists[i].album.name
+            console.log(info)
+
+            logging('spotify-this-song', title, info)
+        }
+        //console.log(songListsObj)
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+}
+
+
+function myMovie(){
+    if(nameOfSongOrMovie){
+        movieFunc(nameOfSongOrMovie)
+        
+    } else {
+        movieFunc(globalOrder)
+    }
+
+    
+}
+
+function movieFunc(title) {
+    var url = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece"
+    
+    request.get({
+        method: "GET",
+        url: url
+    }, function(error, response, body){
+        if(error){
+            console.log(error)
+        } else {
+            if(body){
+                var json = JSON.parse(body)
+                var info = "Title: " + json.Title +
+                "\nYear: " + json.Year + 
+                "\nIMDB Rating: " + json.imdbRating + 
+                "\nRotten Tomatoes Rating: " + json.Ratings[1].Value +
+                "\nCountry: " + json.Country + 
+                "\nLanguage: " + json.Language +
+                "\nPlot: " + json.Plot + 
+                "\nActors: " + json.Actors
+                console.log(info)
+                logging('movie-this', title, info)
+
+            } else {
+                var substitute = "Mr.Nobody"
+                url = "https://www.omdbapi.com/?t=" + substitute + "&y=&plot=short&apikey=40e9cece"
+
+                request.get(url, function(error, response, body){
+                    if(error){
+                        console.log(error)
+                    } else {
+                        let json = JSON.parse(body)
+                        var info = "Title: " + json.Title +
+                        "\nYear: " + json.Year + 
+                        "\nIMDB Rating: " + json.imdbRating + 
+                        "\nRotten Tomatoes Rating: " + json.Ratings[1].Value +
+                        "\nCountry: " + json.Country + 
+                        "\nLanguage: " + json.Language +
+                        "\nPlot: " + json.Plot + 
+                        "\nActors: " + json.Actors
+                        console.log(info)
+                        logging('movie-this', substitute, info)
+
+                    }
+                })
+
+            }
+        }
+        
+    })
+
+}
+
+function doWhatItSay() {
+    fs.readFile('random.txt', 'utf8', function(error, data){
+        if(error){
+            return console.log(error)
+        } else {
+            var dataArray = data.split(',')
+            
+            var commend = dataArray[0]
+            var what = dataArray[1].substr(1,dataArray[1].length-2)
+            console.log(commend)
+            console.log(what)
+
+            globalOrder = what
+            order(commend)
+        }
+    })
+
+}
+
+function logging(commend, title, info) {
+    fs.appendFile('log.txt', '\n\n\n' + commend + ', ' + title + '\n' + info, function(error){
+        if(error){
+            console.log(error)
+        }
+    })
+
+}
